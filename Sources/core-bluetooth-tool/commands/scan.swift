6712b78e063f8c00452c18bcfff674694bdbdb8b
Dr. Michael Lauer <mickey@vanille-media.de>
2021-06-20 16:42:53 +0200

    scan: start refactoring to prepare for supporting more scan modes

--- Sources/core-bluetooth-tool/commands/scan.swift
+++ Sources/core-bluetooth-tool/commands/scan.swift
@@ -11,36 +11,80 @@ struct Scan: ParsableCommand {
     
     public static let configuration = CommandConfiguration(abstract: "Scan BLE devices")
     
-    @Option(help: "Scan for a specific service UUID")
-    private var uuid: String?
-
-    func run() throws {
-        if let uuid = self.uuid {
-            let service = CBUUID(string: uuid)
-            scanner = Scanner(service: service)
-        } else {
-            scanner = Scanner()
-        }
-        scanner.scan()
-
+    @Argument(help: "Specify an entity to scan for, either service, device, device.service, device.service.characteristic, or device.service.characteristic.descriptor")
+    private var entity: String?
+    
+    func loop() {
         let loop = RunLoop.current
         while loop.run(mode: .default, before: Date.distantFuture) {
             loop.run()
         }
     }
+
+    func run() throws {
+        
+        guard let entity = self.entity, !entity.isEmpty else {
+            // scan for everything
+            scanner.scan()
+            self.loop()
+            return
+        }
+        
+        let components = entity.components(separatedBy: ".")
+        switch components.count {
+            case 1:
+                let a = components[0]
+                switch a.count {
+                    case 4: scanner.command = .scanService(CBUUID(string: a))
+                    case 36: scanner.command = .scanPeripheral(UUID(uuidString: a)!)
+                    default:
+                        print("Argument error: \(a) is not a valid peripheral or service ID")
+                        Foundation.exit(-1)
+                }
+            case 2:
+                fallthrough
+            case 3:
+                fallthrough
+            default:
+                fatalError("???")
+        }
+
+        scanner.scan()
+        self.loop()
+    }
 }
 
 class Scanner: NSObject {
     
+    enum Command: Equatable {
+        case scanAll
+        case scanService(CBUUID)
+        case scanPeripheral(UUID)
+        case scanPeripheralService(CBUUID, CBUUID)
+        case scanPeripheralServiceCharacteristic(CBUUID, CBUUID, CBUUID)
+        case scanPeripheralServiceCharacteristicDescriptor(CBUUID, CBUUID, CBUUID, CBUUID)
+    }
+    
     let queue = DispatchQueue(label: "CoreBluetoothQ")
-    let services: [CBUUID]
+    var services: [CBUUID] = []
+    var peripheralIdentifier: UUID?
+
+    var command: Command = .scanAll {
+        didSet {
+            print("Command \(self.command)")
+            switch self.command {
+                case let .scanService(uuid):
+                    self.services = [uuid]
+                case let .scanPeripheral(uuid):
+                    self.peripheralIdentifier = uuid
+                default:
+                    fatalError("not yet implemented")
+            }
+        }
+    }
 
     var manager: CBCentralManager!
     var peripherals: [UUID: CBPeripheral] = [:]
-
-    init(service: CBUUID? = nil) {
-        self.services = service != nil ? [service!] : []
-    }
     
     func scan() {
         self.manager = CBCentralManager(delegate: self, queue: self.queue)
@@ -57,14 +101,16 @@ extension Scanner: CBCentralManagerDelegate {
             case .resetting:
                 break
             case .unsupported:
-                break
+                print("Error: Bluetooth not supported on this device.")
+                Foundation.exit(-1)
             case .unauthorized:
-                break
+                print("Error: Bluetooth usage not authorized.")
+                Foundation.exit(-1)
             case .poweredOff:
                 print("Error: Bluetooth not powered on. Please power on Bluetooth and try again.")
                 Foundation.exit(-1)
             case .poweredOn:
-                print("Scanning…")
+                print("BLE powered on, scanning...")
                 central.scanForPeripherals(withServices: self.services, options: nil)
                 let peripherals = central.retrieveConnectedPeripherals(withServices: [])
                 peripherals.forEach { self.centralManager(central, didDiscover: $0, advertisementData: [:], rssi: 42.0) }
@@ -74,12 +120,38 @@ extension Scanner: CBCentralManagerDelegate {
     }
 
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+
         let identifier = peripheral.identifier
-        guard let _ = self.peripherals[identifier] else {
-            let name = peripheral.name ?? "(Unknown)"
+        guard self.peripherals[identifier] == nil else { return }
+        
+        let name = peripheral.name ?? "(Unknown)"
+        if self.command == .scanAll {
             print("\(identifier)\t\(name)")
-            self.peripherals[identifier] = peripheral
-            return
+        }
+        self.peripherals[identifier] = peripheral
+
+        if identifier == self.peripheralIdentifier {
+            print("Connecting to \(identifier)...")
+            central.connect(peripheral, options: nil)
         }
     }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+
+        print("\(peripheral.identifier) connected, scanning for services...")
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
+    }
+}
+
+extension Scanner: CBPeripheralDelegate {
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let error = error {
+            print("Can't discover services for \(peripheral): \(error)")
+            Foundation.exit(-1)
+        }
+        print("Did discover services: \(peripheral.services)")
+    }
+    
 }
